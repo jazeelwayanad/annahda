186232d891217ff8086b433a50e4ca1f74632409
Muhammed Jaseel K T <76432669+jazeelwayanad@users.noreply.github.com>
2025-10-24 11:39:33 +0000

    upda

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,9 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import $ from 'jquery';
 
-// alpine
-if(!window.Alpine){
-    window.Alpine = Alpine
-    Alpine.start()
-}
-// jquery
-if(!window.jQuery){
-    window.jQuery = window.$ = $
-}
+// Alpine.js
+window.Alpine = Alpine;
+Alpine.start();
+
+// jQuery
+window.$ = window.jQuery = $;
